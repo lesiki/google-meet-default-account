ddc6b1147f197cfea6df60f1d6d3a177f5acfb6e
Andrey Maslennikov <am@kodix.ru>
2021-05-20 16:15:16 +0300

    [i] add force option for force redirect to correct authuser [i] add more domains for redirect (not only from direct meet)

--- src/options.js
+++ src/options.js
@@ -1,13 +1,18 @@
 saveButton = document.getElementById('save');
 authuserInput = document.getElementById('authuser');
+forceInput = document.getElementById('force');
 
 function saveConfig() {
     chrome.storage.sync.set({
         authuser: authuserInput.value,
+        force: forceInput.checked === true
     }, function() {
         saveButton.setAttribute('class', 'button success');
         saveButton.innerHTML = "Saved";
+
         chrome.extension.getBackgroundPage().authUser = authuserInput.value;
+        chrome.extension.getBackgroundPage().force = forceInput.checked === true;
+
         setTimeout(function() {
             saveButton.removeAttribute('class');
             window.close();
@@ -27,10 +32,13 @@ function validateInput() {
 }
 
 function loadConfig() {
-    var initialValue = chrome.storage.sync.get({
+    chrome.storage.sync.get({
         authuser: '',
+        force: false
     }, function(result) {
         authuserInput.value = result.authuser;
+        forceInput.checked = result.force;
+
         validateInput();
     });
 }
